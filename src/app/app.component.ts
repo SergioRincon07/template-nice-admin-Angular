913cdbc833485b6b4b167f6305a39ac8794e7b22
Sergio David Rincon Murcia <sergiorincon50@gmail.com>
2022-11-26 16:31:27 -0500

    Se adiciona la pagina principal el Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [CommonModule, RouterModule, NavbarComponent, SidebarComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NavbarComponent,
+    SidebarComponent,
+    HomeComponent,
+  ],
 })
 export class AppComponent {
   title = 'template-nice-admin';
