08d4213013d69bfac5b6d63fd8102fae046ea57a
Sergio David Rincon Murcia <sergiorincon50@gmail.com>
2022-11-26 16:11:15 -0500

    Se deja solo los componenetes, es decir trabajar sin modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterModule, NavbarComponent, SidebarComponent],
 })
 export class AppComponent {
   title = 'template-nice-admin';
