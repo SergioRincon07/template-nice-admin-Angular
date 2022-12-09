7540d04cf2964c7b9d52b8178a106eb71997b197
Sergio David Rincon Murcia <sergiorincon50@gmail.com>
2022-12-08 21:21:28 -0500

    Se agrega Modal con dialog de Matirial UI

--- src/app/pages/home/components/modal-product/modal-product.component.ts
+++ src/app/pages/home/components/modal-product/modal-product.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import HomeComponent from '../../home.component';
 
 @Component({
   selector: 'app-modal-product',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
   templateUrl: './modal-product.component.html',
   styles: [],
 })
-export class ModalProductComponent {}
+export class ModalProductComponent {
+  constructor(public dialogRef: MatDialogRef<HomeComponent>) {}
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,23 +3,31 @@ import { CommonModule } from '@angular/common';
 import { HomeService } from './services/home.service';
 import { Product } from 'src/app/shared/models/products';
 import { NgOptimizedImage } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ModalProductComponent } from './components/modal-product/modal-product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule, NgOptimizedImage, MatDialogModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export default class HomeComponent implements OnInit {
   products: Product[] = [];
-  constructor(private homeService: HomeService) {
-    console.log(this.products);
-  }
+  constructor(private homeService: HomeService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.homeService.getAllProducts().subscribe(data => {
       this.products = data;
+    });
+  }
+
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(ModalProductComponent, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
     });
   }
 }
